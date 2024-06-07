250ab21989f8d8a07f45e2f52ffd4e92707af314
Luba23 <159457889+Luba23@users.noreply.github.com>
2024-06-07 21:02:21 +0400

    Update Substraction.cs

--- калькулятор1/Operation/Substraction.cs
+++ калькулятор1/Operation/Substraction.cs
@@ -16,7 +16,7 @@ namespace Calculator1.Operation
         public override double Run(params double[] numbers)
         {
             double result = numbers[0];
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 1; i < numbers.Length; i++)
             {
                 result -= numbers[i];
             }

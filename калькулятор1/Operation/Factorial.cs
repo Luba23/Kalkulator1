9ad21c84f0c59e83f601f900ee6f0c82d9e3c40d
Luba23 <159457889+Luba23@users.noreply.github.com>
2024-06-12 21:30:09 +0400

    Add files via upload

---
+++ калькулятор1/Operation/Factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1.Operation
+{
+        public sealed class Factorial : Operation
+    {
+        public Factorial() : base("Факториал")
+        {
+
+        }
+
+        public override double Run(params double[] numbers)
+        {
+            double result = 1;
+            for (int i = 1; i <= numbers[0]; i++)
+            {
+                result *= i;
+            }
+            return result;
+          
+        }
+    }
+}
---
+++ калькулятор1/Operation/Mod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1.Operation
+{
+    public sealed class Mod : Operation
+    {
+        public Mod() : base("Остаток от деления")
+        {
+
+        }
+
+        public override double Run(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result %= numbers[i];
+            }
+            return result;
+        }
+    }
+}
